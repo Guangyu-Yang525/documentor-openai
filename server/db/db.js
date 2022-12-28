c42accc3c8e017443a5b1087291ca2c5a6a1d067
Guangyu Yang <dukeyang7@gmail.com>
2022-12-28 23:57:05 +1100

    remove confidential database credentials

--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,10 @@ import pkg from 'pg'
 const { Pool } = pkg
 
 export const pool = new Pool ({
-    user: "yangguangyu",
-    host: "localhost",
-    port: 5432,
-    database: "documentor"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
 })
 
--- server/services/services-types.js
+++ server/services/services-types.js
@@ -2,5 +2,5 @@ export const SERVICE_TYPES = {
     GRAMMAR_CORRECTION: 'Correct this to standard English:',
     KEYWORDS: 'Extract keywords from this text:',
     SUMMARIZE: 'Summarize this for a second-grade student:',
-    ESSAY_OUTLINE: 'Create an outline for an essay'
+    ESSAY_OUTLINE: 'Create an outline for an essay',
 }
